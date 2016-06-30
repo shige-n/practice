2fca9ec60b0a5956e80d853e448d1b13061e3b49
shigehara <shigehara@shigehara-no-MacBook-Pro.local>
2016-07-01 02:08:15 +0900

    first commit

---
+++ dotinstall-js1/asset/js/style.js
@@ -0,0 +1,27 @@
+
+console.log('Hello World!')
+
+/*変数：データのラベル*/
+
+////////////////////
+
+//変数名
+var msg;
+
+//代入演算子
+msg = "Hello World!";
+console.log(msg);
+
+////////////////////
+
+//変数宣言と代入を一気に書ける
+var msg = "Hello World!";
+console.log(msg);
+
+////////////////////
+
+//宣言を複数書く
+var msg = "Hello World!",
+      x = 20,
+      y = 10;
+console.log(msg,x+y);
