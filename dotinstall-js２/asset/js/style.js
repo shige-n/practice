3af656bdd2b0b99a14f226c6a3a1bd289a648fe3
shigehara <shigehara@shigehara-no-MacBook-Pro.local>
2016-07-04 00:29:45 +0900

    [add] #数値を使う を追加

---
+++ dotinstall-js２/asset/js/style.js
@@ -0,0 +1,26 @@
+//数値を使う
+
+/*
+演算子
++, -, *, /, %
++= : 代入を伴う演算子
+++ -- : 炭鉱演算子
+*/
+
+var x
+
+x = 10 * 2;
+console.log(x);
+
+x = 10 % 3;
+console.log(x);
+
+// x = x + 5;でも良いが、代入演算子が使える
+x += 5;
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
