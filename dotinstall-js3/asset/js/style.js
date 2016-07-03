2600b228743293dd143ac5d719f06b4571f80f89
shigehara <shigehara@shigehara-no-MacBook-Pro.local>
2016-07-04 00:34:54 +0900

    [add] #文字列を使う を追加

---
+++ dotinstall-js3/asset/js/style.js
@@ -0,0 +1,19 @@
+
+//文字列を使う
+
+var s;
+
+//　\n　改行
+//　\t　タブ
+
+s = "it'\s a pen"
+console.log(s);
+
+s = "hello" + "\tworld"
+console.log(s);
+
+s = "hello" + "\nworld"
+console.log(s);
+
+s = "5" + 5
+console.log(s);
